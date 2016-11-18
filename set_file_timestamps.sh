b4462dbfa514fb16a85abe75e98c0d79f03f286b
Costa Tsaousis <costa@tsaousis.gr>
2016-11-18 08:52:46 +0000

    Fri Nov 18 08:52:46 UTC 2016 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -1018,7 +1018,7 @@
 [ -f 'ipblacklistcloud_recent_30d.ipset' ] && /bin/touch --date=@1479310357 'ipblacklistcloud_recent_30d.ipset'
 [ -f 'ipblacklistcloud_recent_7d.ipset' ] && /bin/touch --date=@1479310357 'ipblacklistcloud_recent_7d.ipset'
 [ -f 'ipblacklistcloud_top.ipset' ] && /bin/touch --date=@1477512515 'ipblacklistcloud_top.ipset'
-[ -f 'iw_spamlist.ipset' ] && /bin/touch --date=@1479455424 'iw_spamlist.ipset'
+[ -f 'iw_spamlist.ipset' ] && /bin/touch --date=@1479459025 'iw_spamlist.ipset'
 [ -f 'iw_wormlist.ipset' ] && /bin/touch --date=@1479282622 'iw_wormlist.ipset'
 [ -f 'jigsaw_attacks.ipset' ] && /bin/touch --date=@1459665960 'jigsaw_attacks.ipset'
 [ -f 'jigsaw_malware.ipset' ] && /bin/touch --date=@1459665962 'jigsaw_malware.ipset'
